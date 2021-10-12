0802bb9e1552bc7c67ec481eb8df8c4b250cc8f7
hampster2018 <shieldse01@yahoo.com>
2021-10-12 14:08:16 -0500

    Modified main.dart - Adds a new color 'surface' for buttons or entry fields of Hex CCCCCC - Adds a new Text style body2 with slightly larger font
    Added text_entry_field.dart
    - Adds the basic layout for the stateful widget TextEntry
    - Still needs decoration work, but the basics are laid out

---
+++ lib/UI/text_entry_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class TextEntry extends StatefulWidget {
+  const TextEntry({Key ? key, this.hint = 'hint'}) : super (key: key);
+
+  final String hint;
+
+  @override
+  _TextEntryState createState() => _TextEntryState();
+}
+
+class _TextEntryState extends State<TextEntry> {
+  late TextEditingController _textController;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController(text: widget.hint);
+  }
+
+  @override
+Widget build(BuildContext context) {
+    return CupertinoTextField.borderless(
+      controller: _textController,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+      ),
+      style: Theme.of(context).textTheme.bodyText2,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import './pages/login_page.dart';
+
 void main () => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,9 +14,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
 
+        // All Theme data can be implemented as
+        // Theme.of(context).colorscheme.background
+        // Where colorscheme is the section
+        // And background is the option choice
+
+        // Can all be adjusted at will,
+        // Still more slots if we need to add more colors or accents
         colorScheme: const ColorScheme.highContrastLight(
+            // Primary green color
             primary: Color.fromRGBO(125, 205, 172, 1.0),
-            secondary: Color.fromRGBO(35, 119, 123, 1)
+            // Other blue color we were using as accent
+            secondary: Color.fromRGBO(35, 119, 123, 1),
+            // A transparent color
+            background: Color.fromRGBO(1, 1, 1, 0.0),
+            // The Button Color
+            surface: Color.fromRGBO(204, 204, 204, 100)
         ),
 
         // Define the default font family
@@ -23,13 +38,20 @@ class MyApp extends StatelessWidget {
         // Define the default 'Text Theme'
         // Specifies certain height, and weights
         textTheme: const TextTheme(
+          // Header Font
           headline1: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+          // Smaller Header Font
           headline2: TextStyle(fontSize: 30.0, fontStyle: FontStyle.italic),
-          bodyText2: TextStyle(fontSize: 16.0, fontFamily: 'Open Sans'),
+          // A smaller body size
+          bodyText1: TextStyle(fontSize: 16.0, fontFamily: 'Open Sans'),
+          // Slightly larger body size
+          bodyText2: TextStyle(fontSize: 20.0, fontFamily: 'Open Sans'),
         ),
       ),
 
-      // Add Home Here
+      // The page your testing
+      // Will eventually be the starting page
+      home: const LoginPage(),
 
     );
   }
