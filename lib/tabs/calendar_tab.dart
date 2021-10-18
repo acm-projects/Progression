a81226305e9e05ff5632a56073e7ce77afef541b
hampster2018 <shieldse01@yahoo.com>
2021-10-17 20:12:53 -0500

    Modified 3 files: calendar_tab.dart, pubspec.lock, pubspec.yaml - Added the Calendar table package - implemented a basic Calendar Tab

--- lib/tabs/calendar_tab.dart
+++ lib/tabs/calendar_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:table_calendar/table_calendar.dart';
 import '../UI/background.dart';
 
 class CalendarTab extends StatelessWidget {
@@ -8,10 +9,25 @@ class CalendarTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack (
-      children: const [
-        Background(),
+      children: [
+        const Background(),
 
-        Text('Calendar')
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Calendar',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+            TableCalendar(
+                focusedDay: DateTime.now(),
+                firstDay: DateTime.utc(2020, 1, 1),
+                lastDay: DateTime.utc(2030, 12, 31),
+            ),
+
+          ],
+        ),
       ],
     );
   }
