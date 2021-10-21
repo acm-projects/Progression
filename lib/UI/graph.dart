42bc5c063eb3057ad0aac8a01414cda7910b29c3
hampster2018 <shieldse01@yahoo.com>
2021-10-20 20:52:41 -0500

    fixed another thing i think

--- lib/UI/graph.dart
+++ lib/UI/graph.dart
@@ -11,26 +11,6 @@ class Graph extends StatelessWidget {
       color: Colors.blue,
       data: [0.2, 0.8, 0.4, 0.7, 0.6],
     ),
-    Feature(
-      title: "Exercise",
-      color: Colors.pink,
-      data: [1, 0.8, 0.6, 0.7, 0.3],
-    ),
-    Feature(
-      title: "Study",
-      color: Colors.cyan,
-      data: [0.5, 0.4, 0.85, 0.4, 0.7],
-    ),
-    Feature(
-      title: "Water Plants",
-      color: Colors.green,
-      data: [0.6, 0.2, 0, 0.1, 1],
-    ),
-    Feature(
-      title: "Grocery Shopping",
-      color: Colors.amber,
-      data: [0.25, 1, 0.3, 0.8, 0.6],
-    ),
   ];
 
 
@@ -43,7 +23,7 @@ class Graph extends StatelessWidget {
       children: <Widget>[
         Container(),
         const Padding(
-          padding: EdgeInsets.symmetric(vertical: 64.0),
+          padding: EdgeInsets.symmetric(vertical: 8.0),
           child: Text(
             "Tasks Track",
             style: TextStyle(
@@ -58,10 +38,8 @@ class Graph extends StatelessWidget {
             features: features,
             labelX: const ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5'],
             labelY: const ['20%', '40%', '60%', '80%', '100%'],
-            showDescription: true,
             graphColor: Colors.white30,
             graphOpacity: 0.2,
-            verticalFeatureDirection: true,
             descriptionHeight: 130,
             size: const Size(50,50)
           ),
