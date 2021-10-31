6134036899ee3f700929fd8f6e638eeede4e4d41
hampster2018 <shieldse01@yahoo.com>
2021-10-31 17:25:10 -0500

    Updated create_account_page.dart with Form similar to Login Page

--- lib/pages/create_account_page.dart
+++ lib/pages/create_account_page.dart
@@ -27,133 +27,136 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
   @override
   Widget build(BuildContext context) {
 
-    return Stack(
-      children: [
-
-        const Background(),
-
-        GestureDetector(
-          child: Container(
-            padding: const EdgeInsets.only(left: 30.0, top: 60.0),
-            transformAlignment: Alignment.topLeft,
-            child: const Icon(CupertinoIcons.arrow_left_circle, size: 45),
-          ),
-          onTap: () {
-            Navigator.pop(context);
-          },
-        ),
-
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+    return Form (
+      key: _formKey,
+        child: Stack(
           children: [
 
-            const Padding(padding: EdgeInsets.symmetric(vertical: 10.0),),
+            const Background(),
 
-            Text(
-              'Create Account',
-              style: Theme.of(context).textTheme.headline1,
+            GestureDetector(
+              child: Container(
+                padding: const EdgeInsets.only(left: 30.0, top: 60.0),
+                transformAlignment: Alignment.topLeft,
+                child: const Icon(CupertinoIcons.arrow_left_circle, size: 45),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              },
             ),
 
-            const Padding(padding: EdgeInsets.symmetric(vertical: 30.0),),
-
-            Text(
-              error,
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
-
-            const Padding(padding: EdgeInsets.symmetric(vertical: 10.0),),
-
-            SizedBox(
-              height: 42,
-              width: 253,
-
-              // Might need to add this color if background isn't already transparent
-              // color: Theme.of(context).colorScheme.background,
-
-              child: TextEntry(hint: 'Email', text: widget.emailText,),
-
-            ),
-
-            const Padding(padding: EdgeInsets.symmetric(vertical: 15.0),),
-
-            SizedBox(
-              height: 42,
-              width: 253,
-
-              // Might need to add this color if background isn't already transparent
-              // color: Theme.of(context).colorScheme.background,
-
-              child: TextEntry(hint: 'Password', text: widget.passwordText,),
-
-            ),
-
-            const Padding(padding: EdgeInsets.symmetric(vertical: 15.0),),
-
-            SizedBox(
-              height: 42,
-              width: 253,
-
-              // Might need to add this color if background isn't already transparent
-              // color: Theme.of(context).colorScheme.background,
-
-              child: TextEntry(hint: 'Confirm Password', text: widget.password2Text,),
-
-            ),
-
-            const Padding(padding: EdgeInsets.symmetric(vertical: 15.0),),
-
-            Row(
+            Column(
               mainAxisAlignment: MainAxisAlignment.center,
-
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(
-                  width: 160,
-                  height: 2,
-                  child: Container(
-                    color: Theme.of(context).colorScheme.onBackground,
 
-                  ),
-                ),
-                const Padding(padding: EdgeInsets.symmetric(horizontal: 4.0)),
+                const Padding(padding: EdgeInsets.symmetric(vertical: 10.0),),
+
                 Text(
-                  'or',
+                  'Create Account',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+
+                const Padding(padding: EdgeInsets.symmetric(vertical: 30.0),),
+
+                Text(
+                  error,
                   style: Theme.of(context).textTheme.bodyText1,
                 ),
-                const Padding(padding: EdgeInsets.symmetric(horizontal: 4.0)),
-                SizedBox(
-                  width: 160,
-                  height: 2,
-                  child: Container(
-                    color: Theme.of(context).colorScheme.onBackground,
 
-                  ),
+                const Padding(padding: EdgeInsets.symmetric(vertical: 10.0),),
+
+                SizedBox(
+                  height: 42,
+                  width: 253,
+
+                  // Might need to add this color if background isn't already transparent
+                  // color: Theme.of(context).colorScheme.background,
+
+                  child: TextEntry(hint: 'Email', text: widget.emailText,),
+
+                ),
+
+                const Padding(padding: EdgeInsets.symmetric(vertical: 15.0),),
+
+                SizedBox(
+                  height: 42,
+                  width: 253,
+
+                  // Might need to add this color if background isn't already transparent
+                  // color: Theme.of(context).colorScheme.background,
+
+                  child: TextEntry(hint: 'Password', text: widget.passwordText,),
+
+                ),
+
+                const Padding(padding: EdgeInsets.symmetric(vertical: 15.0),),
+
+                SizedBox(
+                  height: 42,
+                  width: 253,
+
+                  // Might need to add this color if background isn't already transparent
+                  // color: Theme.of(context).colorScheme.background,
+
+                  child: TextEntry(hint: 'Confirm Password', text: widget.password2Text,),
+
+                ),
+
+                const Padding(padding: EdgeInsets.symmetric(vertical: 15.0),),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+
+                  children: [
+                    SizedBox(
+                      width: 160,
+                      height: 2,
+                      child: Container(
+                        color: Theme.of(context).colorScheme.onBackground,
+
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.symmetric(horizontal: 4.0)),
+                    Text(
+                      'or',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    const Padding(padding: EdgeInsets.symmetric(horizontal: 4.0)),
+                    SizedBox(
+                      width: 160,
+                      height: 2,
+                      child: Container(
+                        color: Theme.of(context).colorScheme.onBackground,
+
+                      ),
+                    ),
+                  ],
+                ),
+
+                const Padding(padding: EdgeInsets.symmetric(vertical: 20.0)),
+
+                PageChangeButton(
+                  key: _formKey,
+                  width: 140.0,
+                  height: 50.0,
+                  text: "Create",
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      dynamic result = await _auth.registerWithEmailAndPassword(widget.emailText.text, widget.passwordText.text);
+                      if (result == null) {
+                        error = 'No user found with that email and password';
+                      }
+                      else {
+                        Navigator.pop(context);
+                      }
+                    }
+                  },
                 ),
               ],
             ),
-
-            const Padding(padding: EdgeInsets.symmetric(vertical: 20.0)),
-
-            PageChangeButton(
-              key: _formKey,
-              width: 140.0,
-              height: 50.0,
-              text: "Create",
-              onPressed: () async {
-                if (_formKey.currentState!.validate()) {
-                  dynamic result = await _auth.registerWithEmailAndPassword(widget.emailText.text, widget.passwordText.text);
-                  if (result == null) {
-                    error = 'No user found with that email and password';
-                  }
-                  else {
-                    Navigator.pop(context);
-                  }
-                }
-              },
-            ),
           ],
-        ),
-      ],
+        )
     );
   }
 }
